15cd289bc13c9227be770e53311ec94cd17dee4e
Felix Dahl <uzzus@student.kit.edu>
2025-08-07 19:09:16 +0200

    Fremder Coder

---
+++ Meldeverzüge_Versuch.R
@@ -0,0 +1,207 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+Covid=read.csv("C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\reporting_triangle-icosari-sari_covid19.csv")
+Influenza=read.csv("C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\reporting_triangle-icosari-sari_influenza.csv")
+RSV=read.csv("C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\reporting_triangle-icosari-sari_rsv.csv")
+sari=read.csv("C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\reporting_triangle-icosari-sari.csv")
+#Zuerst Daten vorverarbeiten und dann eine Methode zur Darstellung schreiben
+#Covid
+Daten_kürzen=function(Daten){                           #Nur die Periode 0-4 wird betrachtet
+  Daten=Daten[,1:which(names(Daten) == "value_4w")]
+  return(Daten)
+}
+#Daten auf vorhandene Daten kürzen 
+Covid=Daten_kürzen(Covid)
+Influenza=Daten_kürzen(Influenza)
+RSV=Daten_kürzen(RSV)
+sari=Daten_kürzen(sari)
+Covid=Covid[c(1:27, 136:189),]
+row.names(Covid)=NULL
+Influenza=Influenza[1:27,]
+RSV=RSV[1:27,]
+Covid_gesamt=matrix(0,nrow = (length(Covid[,1])/3),ncol = length(Covid[1,]))
+Covid_gesamt=Covid[1:27,]
+sari_gesamt=sari[1:119,]
+#Stratifizierung der Sari daten raus nehmen
+for (i in 1:119){
+  for(j in which(names(sari)=="value_0w"):which(names(sari)=="value_4w")){
+    sari_gesamt[i, j] <- sum(sari[which(sari$date == sari$date[i]), j], na.rm = TRUE)
+  }
+}
+write.csv(sari_gesamt,"sari_ges.csv",row.names = TRUE)
+read.csv("C:\\Users\\felix\\Desktop\\Uni\\BA\\Code\\eigener Code\\sari_ges.csv")
+#Jetzt noch die Stratifizierung der Covid Daten raus nehmen
+for(i in 1:(length(Covid[,1])/3)){
+  for(j in which(names(Covid)=="value_0w"):which(names(Covid)=="value_4w")){
+    Covid_gesamt[i,j]=Covid[i,j]+Covid[i+27,j]+Covid[i+(2*27),j]
+  }
+}
+i=1
+j=1
+#NA weg machen Setzt NA künstlich auf 0
+NA_Weg=function(Daten){
+  for (i in 1:length(Daten[,1])) {
+    for (j in 1:length(Daten[1,])){
+      if (is.na(Daten[i,j])){
+        Daten[i,j]=0
+      }
+    }
+  }
+  return(Daten)
+}
+Covid_gesamt=NA_Weg(Covid_gesamt)
+Influenza=NA_Weg(Influenza)
+RSV=NA_Weg(RSV)
+sari_gesamt=NA_Weg(sari_gesamt)
+Covid_gesamt=Covid_gesamt[,c(1,3:10)]
+Influenza=Influenza[,c(1,3:10)]
+RSV=RSV[,c(1,3:10)]
+sari_gesamt=sari_gesamt[,c(1,3:10)]
+write.csv(Covid_gesamt,"C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\Covid_reg.csv",row.names = FALSE)
+write.csv(Influenza,"C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\Influenza_reg.csv",row.names = FALSE)
+write.csv(RSV,"C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\RSV_reg.csv",row.names=FALSE)
+write.csv(sari_gesamt[93:119,],"C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\sari_reg.csv",row.names=FALSE)
+#Gesamt Verzüge berechnen
+
+Gesamtverzüge=Covid_gesamt
+Woche0=which(names(Gesamtverzüge)=="value_0w")
+Woche4=which(names(Gesamtverzüge)=="value_4w")
+for(i in 1:length(Gesamtverzüge[,1])){
+  for(j in Woche0:Woche4){
+    Gesamtverzüge[i,j]=Covid_gesamt[i,j]+RSV[i,j]+Influenza[i,j]
+  }
+}
+write.csv(Gesamtverzüge,"C:\\Users\\felix\\Desktop\\Uni\\BA\\Daten\\gesamtverzüge_reg.csv",row.names = FALSE)
+#Daten zum Plotten vorbereiten
+Summe_ber=function(Daten,länge){
+  summe=numeric(länge)
+  summe=rowSums(Daten[,which(names(Daten)=="value_0w"):which(names(Daten)=="value_4w")])  
+  return(summe)
+}
+summe_Covid=Summe_ber(Covid_gesamt,length(Covid_gesamt[,1]))
+summe_Influenza=Summe_ber(Influenza,length(Influenza[,1]))
+summe_RSV=Summe_ber(RSV,length(RSV[,1]))
+summe_Gesamtverzüge=Summe_ber(Gesamtverzüge,length(Gesamtverzüge))
+summe_sari=Summe_ber(sari_gesamt, length(sari_gesamt))
+
+Anteil=function(Daten,Summe){
+  Woche0=which(names(Daten)=="value_0w")
+  Woche4=which(names(Daten)=="value_4w")
+  for(i in 1:length(Daten[,1])){
+    for(j in Woche0:Woche4){
+      if(Summe[i]!=0)
+        Daten[i,j]=Daten[i,j]/Summe[i]
+      else
+        Daten[i,j]=0
+    }
+  }
+  return(Daten)
+}
+Anteil_Covid=Anteil(Covid_gesamt,summe_Covid)
+Anteil_Influenza=Anteil(Influenza,summe_Influenza)
+Anteil_RSV=Anteil(RSV,summe_RSV)
+Anteil_Gesamtverzüge=Anteil(Gesamtverzüge,summe_Gesamtverzüge)
+Anteil_sari=Anteil(sari_gesamt,summe_sari)
+Anteil_sari_gekürzt=matrix(0,nrow = 27,ncol=length(Influenza[1,]))
+Anteil_sari_gekürzt <- Anteil_sari[Anteil_sari$date %in% Influenza$date, ]
+row.names(Anteil_sari_gekürzt)=NULL
+#Daten vorverarbeitung abgeschlossen
+#Plotten
+plotten=function(Anteil_x, Name){
+  df=data.frame(
+    Woche=Anteil_x$date,
+    Wert_Woche0=Anteil_x$value_0w,
+    Wert_Woche1=Anteil_x$value_1w,
+    Wert_Woche2=Anteil_x$value_2w,
+    Wert_Woche3=Anteil_x$value_3w,
+    Wert_Woche4=Anteil_x$value_4w
+  )
+  wochen_labels <- Anteil_x$date[seq(1, length(Anteil_x$date), by = 2)]
+  df_long=df %>%
+    pivot_longer(cols = -Woche,names_to = "Kategorie",values_to = "Wert")
+  df_long$Kategorie = factor(df_long$Kategorie, levels = c("Wert_Woche4", "Wert_Woche3", "Wert_Woche2", "Wert_Woche1", "Wert_Woche0"))    #hierüber lässt sich Reihenfolge steuern
+  df_long$Woche=factor(df_long$Woche, levels=rev(unique(df$Woche)))
+  levels(df_long$Kategorie)
+  ggplot(df_long, aes(x = Woche, y = Wert, fill = Kategorie)) +
+    geom_bar(stat = "identity") +
+    scale_x_discrete(breaks = wochen_labels)+
+    labs(title = paste("Meldeverzüge für ",Name," Anteilig nach Kalenderwoche"),
+         x = "Kalenderwoche",
+         y = "Anteil",
+         fill = "Kategorie") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1,size = 8))
+}
+plotten(Anteil_Covid, "Covid")
+plotten(Anteil_Influenza, "Influenza")
+plotten(Anteil_RSV,"RSV")
+plotten(Anteil_Gesamtverzüge,"Gesamtverzüge")
+plotten(Anteil_sari_gekürzt,"sari")
+plotten = function(Anteil_x1, Anteil_x2) {
+  df1 = data.frame(
+    Woche = Anteil_x1$date,
+    Wert_Woche4 = Anteil_x1$value_4w,
+    Wert_Woche3 = Anteil_x1$value_3w,
+    Wert_Woche2 = Anteil_x1$value_2w,
+    Wert_Woche1 = Anteil_x1$value_1w,
+    Wert_Woche0 = Anteil_x1$value_0w
+  ) %>%
+    pivot_longer(cols = -Woche, names_to = "Kategorie", values_to = "Wert") %>%
+    mutate(Datensatz = "Datensatz1")
+  
+  df2 = data.frame(
+    Woche = Anteil_x2$date,
+    Wert_Woche4 = Anteil_x2$value_4w,
+    Wert_Woche3 = Anteil_x2$value_3w,
+    Wert_Woche2 = Anteil_x2$value_2w,
+    Wert_Woche1 = Anteil_x2$value_1w,
+    Wert_Woche0 = Anteil_x2$value_0w
+    
+  ) %>%
+    pivot_longer(cols = -Woche, names_to = "Kategorie", values_to = "Wert") %>%
+    mutate(Datensatz = "Datensatz2")
+  
+  df_combined = bind_rows(df1, df2)
+  
+  # Neue Variable für Färbung
+  df_combined = df_combined %>%
+    mutate(
+      Kategorie = factor(Kategorie, levels = c("Wert_Woche4", "Wert_Woche3", "Wert_Woche2", "Wert_Woche1", "Wert_Woche0")),
+      Woche_Datensatz = paste0(Woche, "_", Datensatz),
+      Kategorie_Datensatz = paste0(Kategorie, "_", Datensatz)
+    )
+  df_combined$Kategorie = factor(df_combined$Kategorie, levels = c("Wert_Woche4", "Wert_Woche3", "Wert_Woche2", "Wert_Woche1", "Wert_Woche0"))
+  # Farben definieren
+  farben = c(
+    "Wert_Woche4_Datensatz1" = "#DA70D6",  # orchidee
+    "Wert_Woche3_Datensatz1" = "#FFD700",  # gold
+    "Wert_Woche2_Datensatz1" = "#90EE90",  # hellgrün
+    "Wert_Woche1_Datensatz1" = "#87CEFA",  # hellblau
+    "Wert_Woche0_Datensatz1" = "#FFB6C1",  # hellrot
+    "Wert_Woche4_Datensatz2" = "#9932CC",   # dunkellila
+    "Wert_Woche3_Datensatz2" = "#FFA500",  # orange
+    "Wert_Woche2_Datensatz2" = "#228B22",  # waldgrün
+    "Wert_Woche1_Datensatz2" = "#4682B4",  # stahlblau
+    "Wert_Woche0_Datensatz2" = "#DC143C"   # dunkelrot
+  )
+  
+  ggplot(df_combined, aes(x = Woche_Datensatz, y = Wert, fill = Kategorie_Datensatz)) +
+    geom_bar(stat = "identity", width = 0.8) +
+    scale_fill_manual(values = farben) +
+    scale_x_discrete(
+      labels = rep(unique(df_combined$Woche), each = 2)
+    ) +
+    labs(
+      title = "Meldeverzüge anteilig nach Kalenderwoche",
+      x = "Kalenderwoche",
+      y = "Anteil",
+      fill = "Kategorie"
+    ) +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 90, hjust = 1, size = 8)
+    )
+}
+plotten(Anteil_Gesamtverzüge,Anteil_sari_gekürzt)
+
---
+++ nowcasting.R
@@ -0,0 +1,128 @@
+# Apply the KIT-simple_nowcast baseline model to age-stratified data.
+# Author: Johannes Bracher, johannes.bracher@kit.edu
+
+# the paths refer to the root of the repository https://github.com/KITmetricslab/RESPINOW-Hub
+# setwd("/home/johannes/Documents/RESPINOW/RESPINOW-Hub")
+
+
+# Set locale to English, otherwise there will be some unnecessary warnings
+# (when weekdays are checked)
+Sys.setlocale(category = "LC_TIME", locale = "en_US.UTF8")
+# note: this is the Linux version of the command, likely different for Windows
+
+# path of the repo:
+path_repo <- "."
+
+# get functions:
+source(paste0(path_repo, "/code/baseline/functions.R"))
+source(paste0(path_repo, "/respinow_viz/functions.R"))
+
+
+# define data sources (you need just icosari):
+data_source <- "icosari"
+
+# diseases per data source.
+diseases <- c("sari")
+# will later likely become:
+# diseases <- c("sari", "sari_covid", "sari_influenza", "sari_rsv")
+
+
+
+# dates for which to produce nowcasts:
+# for retrospective generation:
+# forecast_dates <- seq(from = as.Date("2024-10-03"),
+#                      to = as.Date("2024-10-10"),
+#                      by = 7)
+# or select an individual forecast_date:
+forecast_dates <- as.Date("2025-03-06")
+
+# set the sizes of training data sets
+n_history_dispersion <- 15
+n_history_expectations <- 15
+max_delay <- 4
+max_horizon <- 3
+
+# specify if plots are to be generated for each nowcast:
+plot_all <- TRUE
+
+# read in reporting triangles:
+triangles <- targets <- list()
+for (disease in diseases) {
+  # note: we load raw reporting triangles, preprocessing takes place inside compute_nowcast
+  triangles[[disease]] <- read.csv(paste0(path_repo, "/data/", data_source, "/", disease, "/",
+                                          "reporting_triangle-", data_source, "-", disease, ".csv"),
+                                   colClasses = c("date" = "Date"), check.names = FALSE)
+  
+  # read in target time series:
+  targets[[disease]] <- read.csv(paste0(path_repo, "/data/", data_source, "/", disease, "/",
+                                        "target-", data_source, "-", disease, ".csv"),
+                                 colClasses = c("date" = "Date"), check.names = FALSE)
+  # this is just the current version of the data in time series format
+}
+
+
+
+# run over forecast dates to generate nowcasts:
+for(i in seq_along(forecast_dates)){
+  forecast_date <- forecast_dates[i]
+  cat(as.character(forecast_dates[i]), "\n")
+  
+  # run over diseases:
+  for (disease in diseases) {
+    
+    # generate nowcasts for age group 00+
+    # note: pre-processing and subsetting of RT takes place inside
+    # note: observed is the reporting triangle for which to generate a nowcast,
+    # observed 2 is the triangle used to estimate the delay pattern (to do this,
+    # borrow_delays and borrow_dispersion need to be set to TRUE)
+    nc <- compute_nowcast(observed = triangles[[disease]], # this is the reporting triangle for which to compute nowcasts
+                          location = "DE",
+                          age_group = "00+",
+                          forecast_date = forecast_date,
+                          observed2 = triangles$sari, # use complete sari to estimate delays
+                          location2 = "DE",
+                          age_group2 = "00+",
+                          type = "additions",
+                          borrow_delays = TRUE,
+                          borrow_dispersion = TRUE,
+                          # note using n_history_expectations_, n_history_dispersion_,
+                          # which may be reduced to fit shorter triangle.
+                          n_history_expectations = n_history_expectations,
+                          n_history_dispersion = n_history_dispersion,
+                          max_delay = max_delay)
+    # note: message "Reporting triangle contains dates later than forecast_date..."
+    # is normal when using the function on earlier dates.
+    
+    # keep only result element:
+    nc <- nc$result
+    
+    # generate a plot if desired:
+    if(plot_all){
+      # truth data as of forecast_date, subset to relevant stratum
+      observed_back_in_time <- data_as_of(dat_truth = triangles[[disease]], date = forecast_date,
+                                          location = "DE", age_group = "00+", max_lag = max_delay)
+      plot_data_back_in_time <- data.frame(date = as.Date(observed_back_in_time$date),
+                                           value = rowSums(observed_back_in_time[, grepl("value_", colnames(observed_back_in_time))],
+                                                           na.rm = TRUE))
+      target_current <- subset(targets[[disease]], age_group == "00+" & location == "DE")
+      
+      plot_forecast(forecasts = nc,
+                    location = "DE", age_group = "00+",
+                    truth = plot_data_back_in_time,
+                    levels_coverage = c(0.5, 0.95),
+                    start = as.Date(forecast_date) - 135,
+                    end = as.Date(forecast_date) + 28,
+                    forecast_date = forecast_date,
+                    ylim = c(0, 1.2*max(tail(plot_data_back_in_time$value, 20)))
+      )
+      # add the most recent data:
+      lines(target_current$date, target_current$value, col = "red", lty  ="solid")
+      title(paste0(disease, ", ", "00+", ", ", forecast_date))
+    }
+    
+    # write out:
+    # need to adapt path if nowcasts shall be written out.
+    # write.csv(nc, file = paste0(path_repo, "/submissions/", data_source, "/", disease, "/KIT-simple_nowcast/",
+    #                             forecast_date, "-", data_source, "-", disease, "-KIT-simple_nowcast.csv"), row.names = FALSE)
+  }
+}
